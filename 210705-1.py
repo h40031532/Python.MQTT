2407e068e3d437240598d9d639d2401e7bf449a3
h40031532 <83333096+h40031532@users.noreply.github.com>
2021-07-06 17:46:30 +0800

    Add files via upload

---
+++ 210705-1.py
@@ -0,0 +1,42 @@
+import paho.mqtt.client as mqtt
+import json, time
+import redis
+import datetime
+import sys
+
+r = redis.StrictRedis(host = '127.0.0.1', port = 6379, decode_responses = True)
+
+##MQTT Connection
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to mqtt server"+str(rc))
+        client.subscribe("test1")
+    else:
+        print("Failed to connected", rc)
+
+#received message from server
+
+def on_message(client, userdata, msg):
+    # print(msg.topic + " " +msg.payload.decode('utf-8'))
+    message = msg.payload.decode('utf-8')
+    # Once msg come into mqtt client, we then store the msg into redis
+    key = datetime.datetime.now()
+    print(f'key : {key}, value : {message} will be store into redis index 0')
+    r.set(key, message)
+    print(r.get(key)) # here you should get message value which you receive in mqtt
+
+def main():
+    int('method1:var')
+    arg1 = sys.argv[1]
+    print(arg1)
+    on_connect()
+    on_message()
+
+#connection setting
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+#set connection info
+client.connect("120.126.16.88", 1883)
+client.loop_forever()
