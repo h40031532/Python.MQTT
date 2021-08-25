d1adcb03e2c4dd82917f3e03a45f7f3d531c4150
h40031532 <83333096+h40031532@users.noreply.github.com>
2021-08-25 21:15:28 +0800

    Update and rename 210707-1.py to 210715-2.py

--- 210715-2.py
+++ 210715-2.py
@@ -11,7 +11,7 @@ r = redis.StrictRedis(host = '127.0.0.1', port = 6379, decode_responses = True)
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to mqtt server"+str(rc))
-        client.subscribe("test1")
+        client.subscribe("test/key1")
     else:
         print("Failed to connected", rc)
 
@@ -20,14 +20,17 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     # print(msg.topic + " " +msg.payload.decode('utf-8'))
     message = msg.payload.decode('utf-8')
+    #message=0
     # Once msg come into mqtt client, we then store the msg into redis
-    key = datetime.datetime.now()
-    print(f'key : {key}, value : {message} will be store into redis index 0')
+    key = str(datetime.datetime.now())
+    print('key : {%s}, value : {%s} will be store into redis index 0' %(key,message))
     r.set(key, message)
     print(r.get(key)) # here you should get message value which you receive in mqtt
+    
 
 def on_publish(client, userdata, mid):
     print("mid: "+str(mid))
+
 
 #connection setting
 client = mqtt.Client()
@@ -38,3 +41,4 @@ client.on_publish = on_publish
 #set connection info
 client.connect("120.126.16.88", 1883)
 client.loop_forever()
+
